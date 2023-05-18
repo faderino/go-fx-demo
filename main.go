bfb5061b16a35a74915efb9a716cfc80235b0636
fadi <fadi99.email@gmail.com>
2023-05-18 21:02:09 +0700

    go dependency injection with fx

---
+++ main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+
+	"go.uber.org/fx"
+	"go.uber.org/fx/fxevent"
+	"go.uber.org/zap"
+)
+
+func main() {
+	app := fx.New(
+		fx.WithLogger(func(log *zap.Logger) fxevent.Logger {
+			return &fxevent.ZapLogger{Logger: log}
+		}),
+		fx.Provide(
+			AsRoute(NewEchoHandler),
+			AsRoute(NewHelloHandler),
+			fx.Annotate(
+				NewServeMux,
+				fx.ParamTags(`group:"routes"`),
+			),
+			NewHTTPServer,
+			zap.NewExample,
+		),
+		fx.Invoke(func(*http.Server) {}),
+	)
+	app.Run()
+}
+
+func NewHTTPServer(lc fx.Lifecycle, mux *http.ServeMux, log *zap.Logger) *http.Server {
+	srv := &http.Server{Addr: ":8080", Handler: mux}
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			ln, err := net.Listen("tcp", srv.Addr)
+			if err != nil {
+				return err
+			}
+			log.Info("Starting HTTP server", zap.String("addr", srv.Addr))
+			go srv.Serve(ln)
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			return srv.Shutdown(ctx)
+		},
+	})
+	return srv
+}
+
+func NewServeMux(routes []Route) *http.ServeMux {
+	mux := http.NewServeMux()
+	for _, route := range routes {
+		mux.Handle(route.Pattern(), route)
+	}
+	return mux
+}
+
+type Route interface {
+	http.Handler
+
+	Pattern() string
+}
+
+func AsRoute(f any) any {
+	return fx.Annotate(
+		f,
+		fx.As(new(Route)),
+		fx.ResultTags(`group:"routes"`),
+	)
+}
+
+type EchoHandler struct {
+	log *zap.Logger
+}
+
+func NewEchoHandler(log *zap.Logger) *EchoHandler {
+	return &EchoHandler{log: log}
+}
+
+func (h *EchoHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if _, err := io.Copy(w, r.Body); err != nil {
+		h.log.Warn("Failed to handle request", zap.Error(err))
+	}
+}
+
+func (*EchoHandler) Pattern() string {
+	return "/echo"
+}
+
+type HelloHandler struct {
+	log *zap.Logger
+}
+
+func NewHelloHandler(log *zap.Logger) *HelloHandler {
+	return &HelloHandler{log: log}
+}
+
+func (h *HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		h.log.Error("Failed to read request", zap.Error(err))
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	if _, err := fmt.Fprintf(w, "Hello, %s\n", body); err != nil {
+		h.log.Error("Failed to write response", zap.Error(err))
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+}
+
+func (*HelloHandler) Pattern() string {
+	return "/hello"
+}
